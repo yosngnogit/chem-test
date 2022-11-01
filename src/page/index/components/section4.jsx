6e98ed630e1be34791d254bcfa53a12039a7fe48
chenzuo <chenzuo@supcon.com>
2022-11-01 15:22:34 +0800

    feat: index section1

---
+++ src/page/index/components/section4.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Section4() {
+  return (
+    <div className='section section4'>
+    </div>
+  )
+}
--- src/page/index/index.jsx
+++ src/page/index/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Carousel } from 'antd';
+
+import './index.less'
+
 import Header from '@/cmponents/header'
+import Section1 from './components/section1';
+import Section2 from './components/section2';
+import Section3 from './components/section3';
+import Section4 from './components/section4';
 
 export default function Index() {
+  const [ active, setActive ] = useState(0)
+
+  const onCarouselChange = (from, to) => {
+    setActive(to)
+  }
+
   return (
     <div className='index'>
-      <Header/>
+      <Header transparent={active=== 0}/>
+      <Carousel className="index-carousel" dotPosition="right" beforeChange={onCarouselChange}>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+      </Carousel>
+      <img className="index-arrow" src={require('@/assets/img/index/arrow.png')}/>
     </div>
   )
 }
