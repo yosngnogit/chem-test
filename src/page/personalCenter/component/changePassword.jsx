5d230859d164ea2f28aed0475934067f2b452bf2
tangwei <tangwei1@supcon.com>
2022-11-02 16:09:49 +0800

    个人中心页面开发

--- src/page/personalCenter/component/changePassword.jsx
+++ src/page/personalCenter/component/changePassword.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Button, Avatar } from 'antd';
+import React, { useState, useEffect, useRef } from 'react'
+
+// import { Button, Avatar } from 'antd';
 import { withRouter } from "react-router-dom";
+import { Button, Form, Input, Avatar } from "antd";
+import md5 from 'js-md5'
 
-// import './index.less'
+import { getCookie } from '@/utils'
+import { pwdReg } from '@/utils/reg'
+import { sendCode, checkMessageCode, modifyPassword } from '@/api/personalCenter'
+import { logOut } from '@/api/login';
 
-function baseInfo(props) {
-  // const onListItemClick = (type) => {
-  //   // 用路由定义type
-  //   props.history.push('baseInfoDetails')
+import '../index.less'
+
+const mobile = getCookie('mobile')
+
+function ChangePassword(props) {
+  const [active, setActive] = useState(0)
+  const [step1Form, setSstep1Form] = useState({})
+  // const step0Ref = useRef();
+
+
+  // const aaa = () => {
+  //   console.log('step0Ref', step0Ref)
   // }
+
   return (
     <div className='personalCenter'>
       <div className="personal-header">
@@ -16,33 +31,206 @@ function baseInfo(props) {
         <div className="personal-title">中控技术股份有限公司</div>
       </div>
       <div className="personal-main">
-        <ul className='main-list'>
-          <li>
-            <div className="info-title">
-              <p>修改密码</p>
-            </div>
-            <div className="info-btn">
-              <Button type="link" >
-                查看
-              </Button>
-            </div>
-          </li>
-          <li>
-            <div className="info-title">
-              <p>修改密码</p>
-            </div>
-            <div className="info-btn">
-              <Button type="link" >
-                修改
-              </Button>
-            </div>
-          </li>
-        </ul>
+        <div className='change-password'>
+          {active === 0 && <Step0 nextStep={setActive} setSstep1Form={setSstep1Form} />}
+          {active === 1 && <Step1 step1Form={step1Form} />}
+        </div>
       </div>
-      <div className="personal-footer">
-        <div className='logout'>退出登录</div>
-      </div>
+      {/* <div className="personal-footer">
+        <div className='logout' onClick={aaa}>下一步</div>
+      </div> */}
     </div>
   )
+
+
 }
-export default withRouter(baseInfo)
+let timerID = 0
+function Step0(props) {
+  const { nextStep, setSstep1Form } = props
+  const [form] = Form.useForm()
+  const [counter, setCounter] = useState(60)
+  const [requestId, setRequestId] = useState('')
+  const [loadingLogin, setLoadingLogin] = useState(false);
+
+  useEffect(() => {
+    clearInterval(timerID)
+    return () => clearInterval(timerID)
+  }, [])
+
+  useEffect(() => {
+    if (counter <= 0 || counter >= 60) {
+      clearInterval(timerID)
+      timerID = 0
+      setCounter(60)
+    }
+  }, [counter])
+
+  const getSmsCode = async () => {
+    try {
+      if (timerID) return
+      timerID = true //设置一个true值，防止重复点击
+      let { mobile } = form.getFieldsValue()
+      let res = await sendCode(mobile)
+      setRequestId(res.data.requestId)
+      timerID = setInterval(() => {
+        setCounter(pre => pre - 1)
+      }, 1000);
+    } catch (err) {
+      timerID = 0
+      throw err
+    }
+  }
+
+  const onFinish = async (values) => {
+    try {
+      if (loadingLogin) return
+      setLoadingLogin(true)
+      await checkMessageCode({
+        requestId,
+        ...values,
+      })
+      setLoadingLogin(false)
+      setSstep1Form({ mobile: values.mobile })
+      nextStep(1)
+    } catch (err) {
+      setLoadingLogin(false)
+      throw err
+    }
+  }
+
+  return (
+    <Form
+      onFinish={onFinish}
+      autoComplete="off"
+      form={form}
+      initialValues={{
+        mobile
+      }}
+      labelCol={{
+        span: 8
+      }}
+    >
+      <Form.Item
+        name="mobile"
+        label="确认企业名称"
+        rules={[
+          { required: true },
+        ]}
+      >
+        <Input disabled />
+      </Form.Item>
+      <Form.Item
+        name="mobile"
+        label="确认绑定手机号"
+        rules={[
+          { required: true, message: "请输入手机号" },
+        ]}
+      >
+        <Input disabled />
+      </Form.Item>
+
+      <Input.Group compact>
+        <Form.Item
+          name="code"
+          label="验证码"
+          rules={[
+            { required: true, message: "请输入验证码" },
+          ]}
+        >
+          <Input
+            placeholder="请输入验证码"
+            maxLength={6}
+          />
+        </Form.Item>
+
+        <Button onClick={getSmsCode} type='link'>
+          {counter > 0 && counter < 60 ? `${counter}s后重新获取` : '发送验证码'}
+        </Button>
+      </Input.Group>
+
+      {/* <Form.Item className='footer'> */}
+      {/* <div className="change-password_btns">
+          <Button type="primary" htmlType="submit">下一步</Button>
+        </div> */}
+      <div className="password-footer">
+        <div className='logout' onClick={form.submit()}>下一步</div>
+      </div>
+      {/* </Form.Item> */}
+    </Form>
+  )
+}
+
+
+function Step1(props) {
+  // const navigate = useNavigate();
+  let { step1Form } = props
+  const [form] = Form.useForm()
+  const [loadingLogin, setLoadingLogin] = useState(false);
+
+  const onFinish = async (values) => {
+    try {
+      if (loadingLogin) return
+      setLoadingLogin(true)
+      await modifyPassword({
+        mobile: step1Form.mobile,
+        newPassword: md5(values.newPassword),
+        rePassword: md5(values.rePassword)
+      })
+      await logOut()
+      // navigate('/login');
+      setLoadingLogin(false)
+    } catch (err) {
+      setLoadingLogin(false)
+      throw err
+    }
+  }
+
+  return (
+    <Form
+      onFinish={onFinish}
+      autoComplete="off"
+      form={form}
+      labelCol={{ span: 5 }}
+      labelAlign='right'
+    >
+      <Form.Item
+        name="newPassword"
+        label="新密码"
+        rules={[
+          { required: true, message: "请输入密码" },
+          { pattern: pwdReg, message: "请输入6-16位大小写、数字、特殊符号组合" },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="rePassword"
+        label="确认密码"
+        rules={[
+          { required: true, message: "请确认密码" },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('newPassword') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('确认密码与密码输入不一致'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      {/* <Form.Item>
+        <div className="change-password_btns">
+          <Button type="primary" htmlType="submit">下一步</Button>
+        </div>
+      </Form.Item> */}
+      <div className="password-footer">
+        <div className='logout' onClick={form.submit()}>下一步</div>
+      </div>
+    </Form>
+  )
+}
+export default withRouter(ChangePassword)
