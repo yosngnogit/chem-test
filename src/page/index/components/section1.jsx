e70d888b684fb09751e4d963c42d162052683f57
chenzuo <348826240@qq.com>
2022-11-09 11:10:38 +0800

    fix: 首页改动

--- src/page/index/components/section1.jsx
+++ src/page/index/components/section1.jsx
@@ -6,17 +6,17 @@ import { getCookie } from '@/utils';
 
 const list = [
   {
-    url: require('@/assets/img/index/section1-test1.png'),
+    url: require('@/assets/img/index/section1-bottom-bg1.png'),
     title: '企业安全自我评测',
     desc: '基于国内安全管理法律法规要求，融入先进跨国公司最佳安全管理实践，运用先进模型推导，科学高效地帮助企业实现安全管理自我评测，并帮助企业找出自身差距，针对性、科学性的去改进现有的安全健康环保管理体系。'
   },
   {
-    url: require('@/assets/img/index/section1-test1.png'),
+    url: require('@/assets/img/index/section1-bottom-bg2.png'),
     title: '企业风险等级评估',
     desc: '综合企业安全自评结果及企业基本信息填报，通过知识库及专家分析评估，挖掘企业风险及隐患，评估企业风险等级。'
   },
   {
-    url: require('@/assets/img/index/section1-test1.png'),
+    url: require('@/assets/img/index/section1-bottom-bg3.png'),
     title: '企业安全管理专家咨询服务',
     desc: '提供国际一流安全专家团队咨询服务，针对安全自诊中的问题，提供针对性的安全问诊服务及科学性的解决方案。'
   },
@@ -51,16 +51,7 @@ function Section1(props) {
                 <div className="section1-carousel-card-desc">{item.desc}</div>
               </div>
             ))}
-          </div>
-          <div className='section1-carousel-item'>
-            {list.map((item, index) => (
-              <div className="section1-carousel-card" key={index}>
-                <img className="section1-carousel-card-img" src={item.url}/>
-                <div className="section1-carousel-card-title">{item.title}</div>
-                <div className="section1-carousel-card-desc">{item.desc}</div>
-              </div>
-            ))}
-          </div>
+          </div>  
         </Carousel>
       </div>
     </div>
--- src/page/index/components/section3.jsx
+++ src/page/index/components/section3.jsx
@@ -5,18 +5,13 @@ export default function Section3() {
   const list = [
     {
       content: '“拥有30多年化学品制造业大型投资项目管理、质量管理、SHE 管理经验。先后在巴斯夫、帝斯曼、杜邦等世界领先的化学品和科学公司担任大中华区、亚太区高级经理、部门总监、大型投资项目经理等职，在麦当劳负责整个中国区的 EHSS 业务。擅长 EHSS 体系的战略性规划、EHSS 组织设计和构建、EHSS 在线平台和数值化实施、行业标准体系的制定和贯彻执行。 ”',
-      avatar: require('@/assets/img/index/section3-avatar.png'),
+      avatar: require('@/assets/img/index/section3-avatar1.png'),
       info: '雷平妹， 化工行业资深安全专家'
     },
     {
-      content: '“拥有30多年化学品制造业大型投资项目管理、质量管理、SHE 管理经验。先后在巴斯夫、帝斯曼、杜邦等世界领先的化学品和科学公司担任大中华区、亚太区高级经理、部门总监、大型投资项目经理等职，在麦当劳负责整个中国区的 EHSS 业务。擅长 EHSS 体系的战略性规划、EHSS 组织设计和构建、EHSS 在线平台和数值化实施、行业标准体系的制定和贯彻执行。 ”',
-      avatar: require('@/assets/img/index/section3-avatar.png'),
-      info: '雷平妹，化工行业资深安全专家'
-    },
-    {
-      content: '“拥有30多年化学品制造业大型投资项目管理、质量管理、SHE 管理经验。先后在巴斯夫、帝斯曼、杜邦等世界领先的化学品和科学公司担任大中华区、亚太区高级经理、部门总监、大型投资项目经理等职，在麦当劳负责整个中国区的 EHSS 业务。擅长 EHSS 体系的战略性规划、EHSS 组织设计和构建、EHSS 在线平台和数值化实施、行业标准体系的制定和贯彻执行。 ”',
-      avatar: require('@/assets/img/index/section3-avatar.png'),
-      info: '雷平妹，化工行业资深安全专家'
+      content: '“29 年石化和化工企业工作经验，在中国石化、BP英国石油、杜邦、伯克希尔哈撒韦集团等国际一流企业负责安全管理、技术和项目管理工作，并担任安全总监和 EHS 总监多年。',
+      avatar: require('@/assets/img/index/section3-avatar2.png'),
+      info: '孙彦东， 资深工艺安全和设备完整性管理专家'
     },
   ]
   return (
