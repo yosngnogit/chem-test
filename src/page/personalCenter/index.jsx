3918aeaebc1d99f0855f5b9cf0b5252feabf13ff
tangwei <tangwei1@supcon.com>
2022-11-11 17:41:23 +0800

    个人中心优化

--- src/page/personalCenter/index.jsx
+++ src/page/personalCenter/index.jsx
@@ -11,37 +11,36 @@ function baseInfo(props) {
   }
   return (
     <div className='personalCenter'>
-      <div className="personal-header">
-        <Avatar shape="square" size={82} src={require('@/assets/img/personalCenter/avatar.png')} />
-        <div className="personal-title">{entName}</div>
+      <div className="center-main">
+        <div className="personal-header">
+          <Avatar shape="square" size={82} src={require('@/assets/img/personalCenter/avatar.png')} />
+          <div className="personal-title">{entName}</div>
+        </div>
+        <div className="personal-main">
+          <ul className='main-list'>
+            <li>
+              <div className="info-title">
+                <p>企业信息</p>
+              </div>
+              <div className="info-btn">
+                <Button type="link" onClick={() => onListItemClick('/personalInfo')} >
+                  查看
+                </Button>
+              </div>
+            </li>
+            <li>
+              <div className="info-title">
+                <p>修改密码</p>
+              </div>
+              <div className="info-btn">
+                <Button type="link" onClick={() => onListItemClick('/changePassword')}>
+                  修改
+                </Button>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="personal-main">
-        <ul className='main-list'>
-          <li>
-            <div className="info-title">
-              <p>企业信息</p>
-            </div>
-            <div className="info-btn">
-              <Button type="link" onClick={() => onListItemClick('/personalInfo')} >
-                查看
-              </Button>
-            </div>
-          </li>
-          <li>
-            <div className="info-title">
-              <p>修改密码</p>
-            </div>
-            <div className="info-btn">
-              <Button type="link" onClick={() => onListItemClick('/changePassword')}>
-                修改
-              </Button>
-            </div>
-          </li>
-        </ul>
-      </div>
-      {/* <div className="personal-footer">
-        <div className='logout'>退出登录</div>
-      </div> */}
     </div>
   )
 }
